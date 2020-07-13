460666ef9d90b1473614d0de68992e50de1657cb
Mestpal <manuestpal@gmail.com>
2020-07-13 17:59:01 +0200

    Vuex available with a small example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
-    Here some data will be shown
+    <div>
+      Here some data will be shown
+    </div>
+
+    <div>
+      Count: {{ $store.state.count }}
+    </div>
+
+    <button @click="increment">
+      Add
+    </button>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+    }
+  }
 }
 </script>
