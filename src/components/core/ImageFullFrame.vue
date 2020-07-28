505f2334124db7e3027501127e8dfbc6e3bfd7a0
Mestpal <manuestpal@gmail.com>
2020-07-28 19:56:21 +0200

    Section title of full frame image component and use of props

--- src/components/core/ImageFullFrame.vue
+++ src/components/core/ImageFullFrame.vue
@@ -1,55 +1,59 @@
 <template>
-  <v-col cols="12">
-    <v-card>
-      <v-hover #default="{ hover }">
-        <v-img
-          v-bind="$attrs"
-          max-height="95vh"
-          max-width="100vw"
-          @load="checkLoad"
-        >
-          <template #placeholder>
-            <v-row>
-              <v-skeleton-loader
-                type="image"
-                min-width="100vw"
-                min-height="95vh"
-              />
-            </v-row>
-          </template>
-
-          <v-row
-            align="end"
-            no-gutters
-          >
-            <v-slide-y-reverse-transition v-if="loaded">
-              <v-overlay
-                v-if="hover"
-                :absolute="true"
-              >
-                <p
-                  class="d-flex darken-4 ma-4"
-                  v-text="$attrs.description"
-                />
-              </v-overlay>
-            </v-slide-y-reverse-transition>
+  <v-card class="ma-0 pa-0">
+    <v-card-title v-if="playerTitle">
+      <h2 v-text="playerTitle" />
+    </v-card-title>
+    <v-hover #default="{ hover }">
+      <v-img
+        :src="src"
+        max-height="80vh"
+        max-width="100vw"
+      >
+        <template #placeholder>
+          <v-row no-gutters>
+            <v-skeleton-loader
+              type="image"
+              min-height="80vh"
+              min-width="100vw"
+            />
           </v-row>
-        </v-img>
-      </v-hover>
-    </v-card>
-  </v-col>
+        </template>
+
+        <v-row
+          align="end"
+          no-gutters
+        >
+          <v-slide-y-reverse-transition>
+            <v-overlay
+              v-if="hover"
+              :absolute="true"
+            >
+              <p
+                class="d-flex darken-4 ma-4"
+                v-text="description"
+              />
+            </v-overlay>
+          </v-slide-y-reverse-transition>
+        </v-row>
+      </v-img>
+    </v-hover>
+  </v-card>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      loaded: false
-    }
-  },
-  methods: {
-    checkLoad () {
-      this.loaded = true
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    playerTitle: {
+      type: String,
+      default: null
     }
   }
 }
