cd4d62beab86f004188a346d3b0c5f2b92f31e23
Mestpal <manuestpal@gmail.com>
2020-08-10 13:53:41 +0200

    First version of modalBase component using .sync

---
+++ src/components/core/modalBase.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-dialog
+      v-model="show"
+      scrollable
+      persistent
+      :overlay="false"
+      max-width="90%"
+      transition="dialog-transition"
+    >
+      <v-card>
+        <v-card-title>
+          <h2 v-text="'playerTitle'" />
+        </v-card-title>
+        <v-card-text>
+          Text
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="success"
+            @click="onClickClose"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    onClickClose () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
