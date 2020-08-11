379afbefe2f97a47055df7a5faf2fa738ad2e445
Mestpal <manuestpal@gmail.com>
2020-08-11 21:45:43 +0200

    full frame image component image modal

---
+++ src/components/molecules/imageFull/imageFullFrameModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <modal-base
+    :show.sync="show"
+    @close="onClickClose"
+  >
+    <template #title>
+      {{ title }}
+    </template>
+    <template #content>
+      <v-img
+        contain
+        aspect-ratio="1.7778"
+        :src="src"
+      />
+    </template>
+    <template #actions>
+      <v-spacer />
+      <v-btn @click.prevent="onClickClose">
+        Close
+      </v-btn>
+    </template>
+  </modal-base>
+</template>
+
+<script>
+import modalBase from '@/components/core/modalBase'
+
+export default {
+  components: {
+    modalBase
+  },
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    onClickClose () {
+      this.$emit('close')
+    },
+    onShowModal () {
+      this.isModalOpen = true
+    }
+  }
+}
+</script>
