afad70ebd8505ae0cf8c3d37812dc99fb6e67251
Mestpal <manuestpal@gmail.com>
2020-08-12 15:37:06 +0200

    Home button styles updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -118,6 +118,7 @@ export default {
           text: 'Previous'
         },
         {
+          color: 'orange',
           condition: this.today !== this.datePickerDate,
           event: 'today',
           text: 'Today'
