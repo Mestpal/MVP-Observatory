380d6be2c1c23227b2fc2a4295f766868ed58b4d
Mestpal <manuestpal@gmail.com>
2020-08-16 13:20:16 +0200

    Navbar now has a navigation drawer with sections

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,43 @@
 <template>
   <v-app>
     <v-app-bar
+      app
       dense
       flat
-      :app="true"
     >
+      <v-app-bar-nav-icon
+        v-if="!isDrawerVisible"
+        @click="updateDrawerVisibility"
+      />
       <v-toolbar-title>MVP Observatory</v-toolbar-title>
-
       <v-spacer />
-
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
     </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="isDrawerVisible"
+      app
+      class="accent-4 deep-purple"
+    >
+      <v-list>
+        <v-list-item>
+          <v-app-bar-nav-icon @click="updateDrawerVisibility" />
+        </v-list-item>
+        <v-list-item
+          v-for="section in drawerSections"
+          :key="section.title"
+          :to="section.route"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ section.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ section.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view />
@@ -42,8 +46,36 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      isDrawerVisible: false,
+      drawerSections: [
+        {
+          icon:'mdi-home',
+          title: 'Home',
+          route: '/'
+        },
+        {
+          icon:'mdi-help-box',
+          title: 'About',
+          route: 'about'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'isMobileBrowser'
+    ]),
+  },
+  methods: {
+    updateDrawerVisibility () {
+      this.isDrawerVisible = !this.isDrawerVisible
+    }
+  },
 };
 </script>
