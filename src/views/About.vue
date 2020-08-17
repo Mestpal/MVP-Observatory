366db60fd344ad15b1e5289d3fdb7dd2c99c128e
Mestpal <manuestpal@gmail.com>
2020-08-17 10:48:24 +0200

    About page info texts

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,30 +2,109 @@
   <v-col>
     <section-intro title="About MVP Observatory Project" />
 
-    <section-info-block
-      :title="infoBlocks[0].title"
-      :subtitle="infoBlocks[0].subtitle"
-      :media="infoBlocks[0].media"
-    >
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Sed eu ultrices turpis, ac fringilla turpis. Donec ut lorem dolor.
-        Duis metus neque, pharetra sit amet lectus id, mattis varius libero.
-        Quisque pulvinar tristique mattis. Nullam nisi sapien
-      </p>
+    <section-info-block>
+      <template #title>
+        <h1>
+          Objective
+        </h1>
+      </template>
+
+      <template #subtitle>
+        <h4 class="font-italic">
+          "Data should be available and easy to understand for everyone, complexity is not equal to wisdom "
+        </h4>
+      </template>
+
+      <template #text>
+        <p>
+          Nowadays is easy to find all the data you need but sometimes is hard to understand or
+          requires a lot of effort to get the meaning of the data you found.
+        </p>
+
+        <p>
+          In the field of astronomy,the amount of public data available is incredible but only
+          a few people can understand it without effort, this is one of the causes many people lost
+          interest in this kind of theme.
+        </p>
+
+        <p>
+          In MVP Observatory we know that, and we are trying to offer you all the data available from
+          space agencies like NASA or ESA in a way that makes you easy to assimilate astronomy info in
+          a user-friendly way
+        </p>
+      </template>
+    </section-info-block>
+
+    <section-info-block :media="true">
+      <template #title>
+        <h1>
+          Technology
+        </h1>
+      </template>
+
+      <template #subtitle>
+        <h3>
+          &#129302;&#128241; &#10145; &#128640;&#128126; &#10145; &#129327;
+        </h3>
+      </template>
+
+      <template #text>
+        <p>
+          The MVP Observatory is a SPA web app, in few words is a front end project that consumes
+          the data from the public API of the different space agencies and show them to you in a pretty way.
+        </p>
+
+        <p>
+          To make this web app we are using many tools but the most important are: <b>Vue.js</b>, a progressive JavaScript framework that allows us to use
+          "components", thanks to this we can reuse a lot of code avoiding a resource expensive and
+          an unmaintainable web app
+        </p>
+
+        <p>
+          For the look&field of the web, we use <b>Vuetify</b> a Material Design Component Framework
+          that facilitate us to give you that look that you are used to see in your devices like your smartphone
+        </p>
+      </template>
     </section-info-block>
 
     <section-info-block
-      :title="infoBlocks[1].title"
-      :subtitle="infoBlocks[1].subtitle"
-      :media="infoBlocks[1].media"
+      :media="true"
+      :inverse="true"
     >
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Sed eu ultrices turpis, ac fringilla turpis. Donec ut lorem dolor.
-        Duis metus neque, pharetra sit amet lectus id, mattis varius libero.
-        Quisque pulvinar tristique mattis. Nullam nisi sapien
-      </p>
+      <template #title>
+        <h1 class="text-right">
+          What's next?
+        </h1>
+      </template>
+
+      <template #subtitle>
+        <h3 class="font-italic text-right">
+          "to the infinity and beyond"
+        </h3>
+      </template>
+
+      <template #text>
+        <p>
+          MVP Observatory is a work in progress project,  where we want to show you all the public data available
+          from all sort of agencies and time to time we will be adding more and more functions to the web app
+
+          In the future, when this web app will have more functionalities we also want to do a backend project
+          that allow us to manage all the data offered by the agencies to improve efficiency and avoid traffic
+          limits and other issues related to the developer's world :D
+
+          Now we are working to improve everything, meanwhile, we hope you enjoy your time in MVP Observatory
+        </p>
+
+        <p>
+          In the future, when this web app will have more functionalities we also want to do a backend project
+          that allow us to manage all the data offered by the agencies to improve efficiency and avoid traffic
+          limits and other issues related to the developer's world &#129299;
+        </p>
+
+        <p>
+          Now we are working to improve everything, meanwhile, we hope you enjoy your time in MVP Observatory
+        </p>
+      </template>
     </section-info-block>
   </v-col>
 </template>
