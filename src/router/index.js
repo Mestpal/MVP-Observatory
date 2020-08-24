385be024b100ad61087d609d4f89c9d0baaf7ae1
Mestpal <manuestpal@gmail.com>
2020-08-24 12:31:12 +0200

    Add nasa routes

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,15 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/nasa',
+    component: () => import('../Index.vue'),
+    children: [
+      { path: '', component: () => import('@/components/core/selectSectionMenu.vue') },
+      { path: 'apod', component: () => import('../views/nasa/Apod.vue') },
+    ]
   }
 ]
 
