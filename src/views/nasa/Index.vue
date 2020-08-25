4fb3fed3058089e7325d79703af147ec4abe67ae
Mestpal <manuestpal@gmail.com>
2020-08-25 16:06:48 +0200

    nasa section index page

---
+++ src/views/nasa/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-col>
+    <section-info-block>
+      <template #title>
+        <h1 class="text-center">
+          Nasa Menu
+        </h1>
+      </template>
+    </section-info-block>
+    <select-section-menu
+      :sections="sections"
+      :hover="true"
+    />
+  </v-col>
+</template>
+
+<script>
+
+import nasaSections from '@/mixins/nasa/sections'
+import sectionInfoBlock from '@/components/core/sectionInfoBlock'
+import selectSectionMenu from '@/components/core/selectSectionMenu'
+
+export default {
+  components: {
+    sectionInfoBlock,
+    selectSectionMenu
+  },
+  mixins:[nasaSections]
+}
+</script>
