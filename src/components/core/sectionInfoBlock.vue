aff1957f9d71e2b323b02b383ae1cbf308c3de60
Mestpal <manuestpal@gmail.com>
2020-08-26 13:37:57 +0200

    Improve visualization sectionInfoBlock and about page

--- src/components/core/sectionInfoBlock.vue
+++ src/components/core/sectionInfoBlock.vue
@@ -1,22 +1,30 @@
 <template>
   <v-row>
-    <v-col v-if="media && inverse">
+    <v-col
+      v-if="media && inverse"
+      cols="12"
+      sm="6"
+    >
       <slot name="media" />
     </v-col>
 
     <v-col>
-      <v-col>
+      <v-col class="pa-0">
         <slot name="title" />
       </v-col>
-      <v-col>
+      <v-col class="pa-0">
         <slot name="subtitle" />
       </v-col>
-      <v-col>
+      <v-col class="pa-0">
         <slot name="text" />
       </v-col>
     </v-col>
 
-    <v-col v-if="media && !inverse">
+    <v-col
+      v-if="media && !inverse"
+      cols="12"
+      sm="6"
+    >
       <slot name="media" />
     </v-col>
   </v-row>
