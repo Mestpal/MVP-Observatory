50dbd608389d5d647f2f663b7cc213343c46f370
Mestpal <manuestpal@gmail.com>
2020-08-31 13:18:12 +0200

    Epic API website in about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -83,11 +83,21 @@
           </p>
 
           <p>
-            Agencies API in use: <br>
+            Agencies API in use:
+            <br>
             <a
               href="https://api.nasa.gov/"
               target="_blank"
-            > NASA API Website</a>
+            >
+              NASA API Website
+            </a>
+            <br>
+            <a
+              href="https://epic.gsfc.nasa.gov/about/api"
+              target="_blank"
+            >
+              NASA EPIC API Website
+            </a>
           </p>
 
           <p>
