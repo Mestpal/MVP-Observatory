091bb20de69a8f31c824314086a6848497fe97a0
Mestpal <manuestpal@gmail.com>
2020-08-31 18:25:35 +0200

    commons mixin

---
+++ src/mixins/commons.js
@@ -0,0 +1,20 @@
+import moment from 'moment'
+import isMobile from 'is-mobile'
+
+
+export default {
+  computed: {
+    checkMobileNavigation () {
+      return isMobile() || this.$vuetify.breakpoint.smAndDown
+    },
+    isLandscape () {
+      return this.checkMobileNavigation && this.$vuetify.breakpoint.smAndUp
+    },
+    previewWitdhMobile () {
+      return this.isLandscape ? "60vw" : "65vw"
+    },
+    today () {
+      return moment().toISOString().substr(0, 10)
+    }
+  }
+}
