6b94186f792908f4e7b1b8d27ea24eab0690137a
Mestpal <manuestpal@gmail.com>
2020-08-31 18:29:18 +0200

    Add overlayInfoMobile in views and commons mixin

--- src/views/nasa/Epic.vue
+++ src/views/nasa/Epic.vue
@@ -14,25 +14,91 @@
         />
       </template>
     </section-info-block>
+
+    <v-row no-gutters>
+      <v-col>
+        <v-carousel :hide-delimiters="true" @change="setActualSlide">
+          <v-carousel-item
+            v-for="(epic, index) in epicData"
+            :key="index"
+          >
+            <image-full-frame
+              v-if="!checkMobileNavigation"
+              :copyright="epic.caption"
+              :description="epic.caption"
+              :src="getUrlImage(epic.image)"
+              :title="epic.date"
+            />
+            <image-full-frame-mobile
+              v-else
+              :copyright="epic.caption"
+              :description="epic.caption"
+              :src="getUrlImage(epic.image)"
+              :title="epic.date"
+              @fullframe="showOverlayImage"
+              @overlay="showOverlay"
+            />
+          </v-carousel-item>
+        </v-carousel>
+        <v-row
+          v-if="actualSlide"
+          align="end"
+          no-gutters
+        >
+          <overlay-info-mobile
+            :show="isOverlayShown"
+            :title="actualSlide.date"
+            @close="hideOverlay"
+          >
+            <template #content>
+              <v-col v-if="actualSlide.caption && !isfullImageVisible">
+                <v-card-text
+                  :class="{textHeight: isLandscape}"
+                  class="darken-4 scrollable text-justify"
+                  v-text="actualSlide.caption"
+                />
+              </v-col>
+              <v-col
+                v-else
+                class="ml-4"
+              >
+                <v-img
+                  contain
+                  :src="getUrlImage(actualSlide.image)"
+                  :width="previewWitdhMobile"
+                />
+              </v-col>
+            </template>
+          </overlay-info-mobile>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-col>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
 import isEmpty from 'lodash/isEmpty'
-import moment from 'moment'
 
-
+import commons from '@/mixins/commons'
 import sectionInfoBlock from '@/components/core/sectionInfoBlock'
 
 export default {
+  name:'Epic',
   components: {
+    imageFullFrame: () => import('@/components/molecules/imageFull/ImageFullFrame'),
+    imageFullFrameMobile: () => import('@/components/molecules/imageFull/ImageFullFrameMobile'),
+    overlayInfoMobile: () => import("@/components/core/overlayInfoMobile"),
     sectionInfoBlock
   },
+  mixins: [commons],
   data() {
     return {
+      actualSlide: null,
       epicTitle: "EPIC: Earth captures of astronomical events",
-      datePickerDate: null
+      datePickerDate: null,
+      isfullImageVisible: false,
+      isOverlayShown: false
     }
   },
   computed: {
@@ -41,11 +107,11 @@ export default {
       'isMobileBrowser',
       'selectedEpicDate'
     ]),
-    checkMobileNavigation () {
-      return this.isMobileBrowser || this.$vuetify.breakpoint.smAndDown
+    imageBaseUrl () {
+      return process.env.VUE_APP_NASA_EPIC_ARCHIVE_BASE_URL
     },
-    today () {
-      return moment().toISOString().substr(0, 10)
+    selectedEpicDateArray () {
+      return this.today.split("-")
     }
   },
   mounted () {
@@ -57,6 +123,23 @@ export default {
       'getEpic',
       'setApodDate'
     ]),
+    getUrlImage(image) {
+      return`${this.imageBaseUrl}/natural/${this.selectedEpicDateArray[0]}/${this.selectedEpicDateArray[1]}/${this.selectedEpicDateArray[2] - 1}/jpg/${image}.jpg`
+    },
+    hideOverlay () {
+      this.isOverlayShown = false
+      this.isfullImageVisible = false
+    },
+    setActualSlide ($event) {
+      this.actualSlide = this.epicData[$event]
+    },
+    showOverlay () {
+      this.isOverlayShown = true
+    },
+    showOverlayImage () {
+      this.isfullImageVisible = true
+      this.showOverlay()
+    }
   }
 }
 </script>
